1494d9525264b4248b12afdabc856bc80cb7f072
giana medeiros <gianamedeiros.00510@gmail.com>
2023-07-10 20:21:44 -0300

    Created project and class client

---
+++ src/main/java/gm/customer/registration/CustomerApplication.java
@@ -0,0 +1,13 @@
+package gm.customer.registration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerApplication.class, args);
+	}
+
+}
