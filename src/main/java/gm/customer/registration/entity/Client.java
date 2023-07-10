5d8798edc3bd991eb9100b002ad9d9dee76a5767
giana medeiros <gianamedeiros.00510@gmail.com>
2023-07-10 20:45:36 -0300

    Database access configuration

--- src/main/java/gm/customer/registration/entity/Client.java
+++ src/main/java/gm/customer/registration/entity/Client.java
@@ -1,17 +1,24 @@
 package gm.customer.registration.entity;
 
+import jakarta.persistence.*;
+
 import java.io.Serial;
 import java.io.Serializable;
 import java.time.Instant;
 import java.util.Objects;
 
+@Entity
+@Table(name = "tb_client")
 public class Client implements Serializable {
     @Serial
     private static  final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String cpf;
     private Double income;
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
     private Instant birthDate;
     private Integer children;
 
